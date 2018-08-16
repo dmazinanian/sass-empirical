a4a482570ac6361854872da9c082fd23ce50044c
dmazinanian <dmazinanian@gmail.com>
2018-08-15 18:23:44 -0700

    Fix finding caller rules

--- empirical/ConstructInfo/sass_mixin_call.rb
+++ empirical/ConstructInfo/sass_mixin_call.rb
@@ -41,44 +41,85 @@ class SassMixinCall < SassConstruct
   end
 
   def get_callers(all_mixin_calls)
-
     callers = []
+    get_caller_rule_nodes(all_mixin_calls).each do |d|
+      begin
+        compiled = Sass.compile(d.to_scss)
+        compiled_ast = Sass::Engine.new(compiled, :syntax => :scss).to_tree
+        rule = compiled_ast.children[0]
+        unless rule.nil?
+          rule.children = []
+          callers << SassASTQueryHandler.remove_illegal_chars(rule.to_sass)
+        else
+          raise Sass::SyntaxError.new("Failed to parse #{d.to_scss}")
+        end
+      rescue Sass::SyntaxError
+        callers << SassASTQueryHandler.remove_illegal_chars(d.to_scss.gsub("prop: val;", "").gsub!(/{|}/, ""))
+      end
+    end
+    puts(callers)
+    callers
+  end
+
+  def get_caller_rule_nodes(all_mixin_calls)
+
+    caller_rules = []
 
     @parents_to_root.each do |parent|
       if parent.kind_of?(Sass::Tree::RuleNode)
-        rule_name = SassASTQueryHandler.remove_illegal_chars(parent.rule.join)
-        if callers.empty?
-          callers << rule_name
+        current = parent.clone
+        if caller_rules.empty?
+          dummy_style_sheet = Sass::Engine.new(".dummy\n\tprop: val").to_tree
+          dummy_declaration = dummy_style_sheet.children[0].children[0]
+          current.children = [dummy_declaration]
+          caller_rules << current
         else
-          callers.each_with_index do |caller, index|
-            callers[index] = rule_name + " " + caller
+          caller_rules.each_with_index do |caller_rule, index|
+            new_parent = current.clone
+            new_parent.children = [caller_rule]
+            caller_rules[index] = new_parent
           end
         end
       elsif parent.kind_of?(Sass::Tree::MixinDefNode)
-        callers_for_mixin = []
+        caller_rules_for_mixin = []
         all_mixin_calls.each do |other_mixin_call|
           if other_mixin_call.mixin_declaration.nil?
             puts "No mixin declaration was found for #{other_mixin_call.mixin_name} in #{@path_to_style_sheet}."
           else
             if other_mixin_call.mixin_declaration.mixin_name.eql?(SassASTQueryHandler.remove_illegal_chars(parent.name))
-              callers_for_mixin.concat(other_mixin_call.get_callers(all_mixin_calls))
+              caller_rules_for_mixin.concat(other_mixin_call.get_caller_rule_nodes(all_mixin_calls))
             end
           end
         end
-        callers_for_mixin.each_with_index do |caller, index|
-          callers.each do |caller_up_to_now|
-            callers_for_mixin[index] = caller + " " + caller_up_to_now
+        new_caller_rules = []
+        caller_rules_for_mixin.each do |caller_for_mixin|
+          caller_rules.each do |caller_rule|
+            new_parent = caller_for_mixin.clone
+            leaf_node = get_leaf(new_parent)
+            leaf_node.children = [caller_rule.clone]
+            new_caller_rules << new_parent
           end
         end
-        callers = callers_for_mixin
-      #elsif parent.kind_of?(Sass::Tree::MediaNode)
-      else
-        puts "The mixin is called in #{parent.class}, skipping to the next parent."
+        caller_rules = new_caller_rules
       end
+
     end
 
-    callers
+    caller_rules
 
+  end
+
+  def get_leaf(node)
+    leaf = node
+    previous_leaf = nil
+    while leaf.children.size > 0
+      previous_leaf = leaf
+      leaf = leaf.children[0]
+    end
+    if leaf.kind_of? Sass::Tree::PropNode
+      leaf = previous_leaf
+    end
+    leaf
   end
 
 end
